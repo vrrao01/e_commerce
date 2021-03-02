0d9491b09fb50728d30aa082981936aeffdb5d67
vrrao <vigneshravirao@gmail.com>
2021-03-03 04:51:55 +0530

    Update order item

--- Ecommerce/store/admin.py
+++ Ecommerce/store/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Product,Category
+from .models import Product,Category,Order,OrderItem
 
 # Register your models here.
 
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- Ecommerce/store/models.py
+++ Ecommerce/store/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from accounts.models import UserProfile
 # Create your models here.
 
 class Category(models.Model):
@@ -18,8 +18,24 @@ class Product(models.Model):
 	price = models.FloatField()
 	image = models.ImageField()
 	category = models.ForeignKey(Category,on_delete=models.SET_NULL, null=True)
-
+	description = models.TextField(max_length=250,default="",null=False)
 
 	def __str__(self):
 		return self.name
 
+class Order(models.Model):
+	customer = models.ForeignKey(UserProfile,on_delete=models.SET_NULL,null=True)
+	completed = models.BooleanField(default=False,null=False,blank=False)
+	transaction_id = models.CharField(max_length=200,null=True)
+
+	def __str__(self):
+		return f"Order-{self.id}"
+
+class OrderItem(models.Model):
+	product = models.ForeignKey(Product,on_delete=models.SET_NULL,null=True)
+	order = models.ForeignKey(Order,on_delete=models.SET_NULL,null=True)
+	quantity = models.IntegerField(default=0,null=True,blank=True)
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f"{self.product.name}-Order-{self.order.id}"
