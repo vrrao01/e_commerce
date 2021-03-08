58983836e6996088d210fd487ebff5f85fa3097e
vrrao <vigneshravirao@gmail.com>
2021-03-08 20:07:00 +0530

    :construction: Badge with cart item count

--- Ecommerce/home/views.py
+++ Ecommerce/home/views.py
@@ -5,11 +5,33 @@ from store.models import Product,Category,Order
 from django.views.generic.detail import DetailView
 from accounts.models import UserProfile
 from django.contrib.auth.models import User
+from store.views import merge_orders
+from django.contrib.auth import get_user
+from store.models import Order
 # Create your views here.
 
 # def home(request):
 # 	return render(request,"home/homepage.html")
 # 	
+
+def fetchcart(request):
+	if (request.user.is_authenticated):
+		curUser = get_user(request)
+		if('cartid' in request.session):
+			cartid = request.session.get('cartid')
+			sessionCart = Order.objects.get(pk = cartid)
+			if(Order.objects.filter(placed = False,customer = curUser).exists()):
+				storedCart = Order.objects.all().filter(placed = False)[0]
+				if(storedCart.id != sessionCart.id):
+					merge_orders(cartid,storedCart.id)
+					request.session['cartid'] = storedCart.id
+			else:
+				sessionCart.customer = curUser
+				sessionCart.save()
+		else:
+			if(Order.objects.filter(placed = False, customer=curUser).exists()):
+				storedOrder = Order.objects.filter(placed = False,customer = curUser)[0]
+				request.session['cartid'] = storedOrder.id
 
 class Homepage(ListView):
 	model = Product
@@ -20,6 +42,11 @@ class Homepage(ListView):
 	def get_context_data(self,**kwargs):
 		context = super().get_context_data(**kwargs)
 		context['categories'] = list(Category.objects.all())
+		fetchcart(self.request)
+		cartcount = 0
+		if('cartid' in self.request.session):
+			cartcount = Order.objects.get(id = self.request.session.get('cartid')).itemcount
+		context['cartcount'] = cartcount
 		return context
 
 class ProductDetail(DetailView):
